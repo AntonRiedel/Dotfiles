6c2a79eafcc6c156b5d9c05cc8eca90515c815e0
Anton <anton.riedel@tum.de>
2019-03-13 17:23:16 +0100

    update

--- shell/.config/shell/alias.sh
+++ shell/.config/shell/alias.sh
@@ -2,7 +2,7 @@
 #     File Name           :     alias.sh
 #     Created By          :     Anton Riedel <anton.riedel@hotmail.com>
 #     Creation Date       :     [2019-03-12 16:54]
-#     Last Modified       :     [2019-03-13 14:34]
+#     Last Modified       :     [2019-03-13 17:23]
 #     Description         :     Aliases for the shell
 ###############################################################################
 
@@ -26,10 +26,10 @@ alias vpn="sudo vpnc"
 
 ##custom commands
 #search and edit a configuration file
-ce() { find $HOME/Dotfiles/* -type f | fzf | xargs -r -I % $EDITOR %; }
-#search and enter a directory in $HOME
-fd() { cd "$(find $HOME/* -type d | fzf)" && pwd && ls; }
+ce() { nvim $(find $HOME/Dotfiles/* -type f | fzf); }
+#search and enter a directory (fails if you lack permissions)
+fd() { cd "$(find / -type d 2>/dev/null | fzf)" && pwd && ls; }
 #search and execute a command from history
 hs() { $(cat "$HOME/.bash_history" | sort -u | fzf); }
 #search for a process and kill it
-kp() { kill "$(ps -e | fzf | awk '{ print $1 }')"; }
+kp() { kill -s SIGRTMIN+15 "$(ps -e | fzf | awk '{ print $1 }')" 2>/dev/null; }
