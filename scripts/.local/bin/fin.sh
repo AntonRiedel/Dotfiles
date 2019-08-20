e8e4f3310bfcd8438085b6cec71d96017b5b6ac8
Anton <anton.riedel@tum.de>
2019-08-20 13:05:01 +0200

    update

--- scripts/.local/bin/fin.sh
+++ scripts/.local/bin/fin.sh
@@ -3,7 +3,7 @@
 #     File Name           :     fin.sh
 #     Created By          :     Anton Riedel <anton.riedel@hotmail.com>
 #     Creation Date       :     [2019-02-23 19:42]
-#     Last Modified       :     [2019-06-04 21:37]
+#     Last Modified       :     [2019-08-20 00:35]
 #     Description         :     Shutdown, reboot or kill current i3 instance
 #################################################################################
 
@@ -17,8 +17,8 @@ pgrep -x dmenu && exit
 choice=$(echo "Shutdown\nReboot\nExit\nLock Screen" | dmenu -i -p "Select Option")
 
 case "$choice" in
-Shutdown) ClearCache && sudo poweroff ;;
-Reboot) ClearCache && sudo reboot ;;
+Shutdown) ClearCache && shutdown now ;;
+Reboot) ClearCache && reboot ;;
 "Exit") killall Xorg ;;
 "Lock Screen") lockscreen.sh ;;
 esac
