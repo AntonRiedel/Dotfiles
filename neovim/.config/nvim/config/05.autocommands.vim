270088cb6652ab7e12dec71574333745710a510e
Anton Riedel <anton.riedel@tum.de>
2020-03-27 15:18:47 +0100

    update

--- neovim/.config/nvim/config/05.autocommands.vim
+++ neovim/.config/nvim/config/05.autocommands.vim
@@ -1,7 +1,7 @@
 " File              : 05.autocommands.vim
 " Author            : Anton Riedel <anton.riedel@tum.de>
 " Date              : 24.03.2020
-" Last Modified Date: 24.03.2020
+" Last Modified Date: 27.03.2020
 " Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 "Reload init.vim if a config file gets changed
--- scripts/.local/bin/sound.sh
+++ scripts/.local/bin/sound.sh
@@ -2,7 +2,7 @@
 # File              : sound.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 24.03.2020
-# Last Modified Date: 26.03.2020
+# Last Modified Date: 27.03.2020
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 [ -z "$2" ] && num="10" || num="$2"
@@ -13,4 +13,5 @@ d*) pulsemixer --change-volume -"$num" ;;
 m*) pulsemixer --toggle-mute ;;
 esac
 
-refbar.sh
+#update statusbar by sending signal 10 do dwmblocks
+pkill -RTMIN+10 dwmblocks
