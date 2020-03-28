9219e6ac1a2d1b3de62bcea86df5bc23b04086cf
Anton Riedel <anton.riedel@tum.de>
2020-03-28 20:31:23 +0100

    update

--- scripts/.local/bin/screen.sh
+++ scripts/.local/bin/screen.sh
@@ -2,7 +2,7 @@
 # File              : screen.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 24.03.2020
-# Last Modified Date: 26.03.2020
+# Last Modified Date: 28.03.2020
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 pgrep -x dmenu && exit
@@ -12,3 +12,7 @@ layout=$(ls $HOME/.config/screenlayout | dmenu -F -i -p "Where are you?")
 $HOME/.config/screenlayout/$layout
 
 xwallpaper --stretch $HOME/.config/wallpaper/default
+
+#keboard setup
+setxkbmap us
+setxkbmap -option caps:escape
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -2,6 +2,7 @@
 # File              : .zprofile
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 24.03.2020
-# Last Modified Date: 24.03.2020
+# Last Modified Date: 28.03.2020
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
+
 source $HOME/.profile
