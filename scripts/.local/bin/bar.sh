6ef55c568b51d45a9c546cd5f9f27d1e30e0e985
Anton Riedel <anton.riedel@tum.de>
2020-04-27 20:53:41 +0200

    Update

--- scripts/.local/bin/bar.sh
+++ scripts/.local/bin/bar.sh
@@ -37,7 +37,7 @@ status() {
 
 	#show remaining battery power
     if [ -d /sys/class/power_supply/BAT? ]; then
-        acpi -b | awk '{print $3 $4 "("$5")"}' | sed -e 's/,//g; s/Charging/🔌/g; s/Discharging/🔋/g; s/Unknown/♻️/g'
+        acpi -b | awk '{print $3 $4 "("$5")"}' | sed -e 's/,//g; s/Charging/🔌/g; s/Full/⚡/g; s/Discharging/🔋/g; s/Unknown/♻️/g'
     else
         echo "⛽"
     fi
