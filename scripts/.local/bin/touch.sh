cb6331c93b6b918daef8a3ce78ae36967b360305
Anton Riedel <anton.riedel@tum.de>
2020-05-02 20:49:44 +0200

    update

--- scripts/.local/bin/touch.sh
+++ scripts/.local/bin/touch.sh
@@ -2,22 +2,22 @@
 # File              : touch.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 04.04.2020
-# Last Modified Date: 28.04.2020
+# Last Modified Date: 02.05.2020
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 if [ "$1" = "setup" ]; then
     flameshot 2>/dev/null &
     easystroke --config-dir $HOME/.config/easystroke 2>/dev/null &
     #cellwriter --profile=$HOME/.config/cellwriter/profile --dock-window=2 --hide-window 2>/dev/null &
-    cellwriter --keyboard-only &
-    notify-send -t 1500 "Touchscreen setup" "Start flameshot\nStart easystroke\nStart cellwriter"
+    #cellwriter --keyboard-only &
+    notify-send -t 1500 "Touchscreen setup" "Start flameshot\nStart easystroke\n"
 elif [ "$1" = "reset" ];then
     killall flameshot 2>/dev/null
     killall easystroke 2>/dev/null
-    killall cellwriter 2>/dev/null
+    #killall cellwriter 2>/dev/null
     xsetroot -name "--RESET--"
     pkill --signal 5 bar.sh
-    notify-send -t 1500 "Touchscreen reset" "Kill flameshot\nKill easystroke\nKill cellwriter"
+    notify-send -t 1500 "Touchscreen reset" "Kill flameshot\nKill easystroke\n"
 else
     notify-send "touch.sh" "Invalid argument"
 fi
