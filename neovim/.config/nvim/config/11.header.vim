896d5f9d89cfedf4f82e7c9106eb9d6b01e00d93
Anton Riedel <anton.riedel@tum.de>
2020-05-06 21:31:34 +0200

    Update

--- neovim/.config/nvim/config/11.header.vim
+++ neovim/.config/nvim/config/11.header.vim
@@ -4,6 +4,7 @@
 " Last Modified Date: 05.04.2020
 " Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
+let g:header_auto_add_header = 0
 let g:header_field_author = 'Anton Riedel'
 let g:header_field_author_email = 'anton.riedel@tum.de'
 let g:header_max_size = 12
--- scripts/.local/bin/nextsync.sh
+++ scripts/.local/bin/nextsync.sh
@@ -2,7 +2,7 @@
 # File              : nextsync.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 24.03.2020
-# Last Modified Date: 19.04.2020
+# Last Modified Date: 06.05.2020
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 #credentials
@@ -13,4 +13,4 @@ dir="$HOME/nextcloud"
 server="https://sgrastar.ddns.net/nextcloud"
 
 #sync files
-nextcloudcmd --user $user --password $password $dir $server
+nextcloudcmd --user $user --password $password --exclude $HOME/.config/Nextcloud/sync-exclude.lst $dir $server
