1ea1f8a46030c90f52ab4519edc34d9927123af8
Anton Riedel <anton.riedel@tum.de>
2021-03-04 11:56:52 +0100

    update

--- scripts/.local/bin/lockscreen.sh
+++ scripts/.local/bin/lockscreen.sh
@@ -5,18 +5,8 @@
 # Last Modified Date: 28.02.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
-#clean up before
-rm -f /tmp/*png
-
-#take a screenshot of the entire desktop and save it under /tmp
-#it will be called 'something.png'
-flameshot full -p /tmp
-
-#wait for flameshot to take the screenshot
-sleep 1
-
-#find and rename the picture
-find /tmp -type f -name "*png" -exec mv {} /tmp/screen.png \;
+#take a screenshot of the entire desktop (with imagemagick)
+import -window root /tmp/screen.png
 
 #blur and swirl the picture
 convert /tmp/screen.png -blur 0x8 -swirl 120 /tmp/locked.png
@@ -24,7 +14,7 @@ convert /tmp/screen.png -blur 0x8 -swirl 120 /tmp/locked.png
 #lock the screen
 i3lock -i /tmp/locked.png
 
-#clean up after
+#clean up afterwards
 rm /tmp/locked.png /tmp/screen.png
 
 exit 0
