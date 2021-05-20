46fc79c65bff7edbdd0e3236e9e4c217c6696d18
Anton Riedel <anton.riedel@tum.de>
2021-05-20 22:42:00 +0200

    update

--- scripts/.local/bin/screen.sh
+++ scripts/.local/bin/screen.sh
@@ -2,7 +2,7 @@
 # File              : screen.sh
 # Author            : Anton Riedel <anton.riedel@tum.de>
 # Date              : 24.03.2020
-# Last Modified Date: 30.11.2020
+# Last Modified Date: 19.05.2021
 # Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 
 pgrep -x dmenu && exit 1
@@ -14,6 +14,11 @@ layout=$(ls $HOME/.config/screenlayout | dmenu -F -i -p "Where are you?")
 $HOME/.config/screenlayout/$layout
 
 #clean up
+setxkbmap -option ctrl:nocaps #map capslock to CRTL
+xcape -e 'Control_L=Escape'   #map CRTL to escape when pressed on its own
+xset -dpms
+xset s off
+xset r rate 300 50
 xwallpaper --stretch $WALLPAPER
 setxkbmap us
 setxkbmap -option caps:escape
