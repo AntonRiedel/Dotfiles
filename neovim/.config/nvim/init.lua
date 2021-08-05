2186f77c83f9e6f8a9f8de0a9c07c2845c2a9e39
Anton Riedel <anton.riedel@tum.de>
2021-08-05 20:35:08 +0200

    update

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,7 +2,7 @@
 File              : init.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 25.04.2021
-Last Modified Date: 01.08.2021
+Last Modified Date: 05.08.2021
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 --]] --
 -- main configuration file
--- neovim/.config/nvim/lua/plugins/indentline.lua
+++ neovim/.config/nvim/lua/plugins/indentline.lua
@@ -2,7 +2,15 @@
 File              : indentline.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 06.06.2021
-Last Modified Date: 03.08.2021
+Last Modified Date: 05.08.2021
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 --]] --
-require("indent_blankline").setup({char = "|", buftype_exclude = {"terminal"}})
+require("indent_blankline").setup({
+    char = "|",
+    use_treesitter = true,
+    -- space_char = "_",
+    -- char_highlight_list = {"Warning", "Error"},
+    -- show_current_context = true,
+    -- context_highligh_list = {"Warning"},
+    buftype_exclude = {"terminal"}
+})
