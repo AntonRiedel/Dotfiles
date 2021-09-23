ed2259f9f36bf8d7861424d471cbab9ee1d6f8a3
Anton Riedel <anton.riedel@tum.de>
2021-09-23 15:33:44 +0200

    update

--- neovim/.config/nvim/lua/plugins/snippets.lua
+++ neovim/.config/nvim/lua/plugins/snippets.lua
@@ -2,7 +2,7 @@
 File              : snippets.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 19.09.2021
-Last Modified Date: 22.09.2021
+Last Modified Date: 23.09.2021
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 --]] --
 --
@@ -27,7 +27,17 @@ ls.snippets = {
             name = "debug",
             dscr = "cout current line",
             docstring = "std::cout << 1 << std::endl"
-        }, {t("std::cout << \"DEBUG: Line \" <<__LINE__ << std::endl;")})
+        }, {t("std::cout << \"DEBUG: Line \" <<__LINE__ << std::endl;")}), s(
+            {
+                trig = "for",
+                name = "Loop",
+                dscr = "for loop",
+                docstring = "for(1;2;3){4}"
+            }, {
+                t("for ("), i(1, "int"), t(" "), i(2, "i"), t("="), i(3, "0"),
+                t(";"), i(4, "i"), i(5, "<"), i(6, "N"), t(";"), i(7, "i++"),
+                t({"){", "   "}), i(8, "LOOP"), t({"", "}"})
+            })
     },
     -- latex snipptes
     tex = {
