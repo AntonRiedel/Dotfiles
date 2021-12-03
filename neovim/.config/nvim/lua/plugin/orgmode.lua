c9821fcf145580f58838304ec449cb10d63fb038
Anton Riedel <anton.riedel@tum.de>
2021-12-03 10:16:45 +0100

    update

--- neovim/.config/nvim/lua/plugin/orgmode.lua
+++ neovim/.config/nvim/lua/plugin/orgmode.lua
@@ -2,9 +2,12 @@
 File              : orgmode.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 30.11.2021
-Last Modified Date: 30.11.2021
+Last Modified Date: 02.12.2021
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
---]]--
+--]] --
 if packer_bootstrap then return end
 
-require('orgmode').setup({org_agenda_files = {'$HOME/nextcloud/org'}})
+require('orgmode').setup({
+    org_agenda_files = {'~/nextcloud/org/*'},
+    org_default_notes_file = '~/nextcloud/org/default.org'
+})
