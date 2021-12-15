448afe69801680dafefbcd2a6384144ecef15e55
Anton Riedel <anton.riedel@tum.de>
2021-12-15 17:15:26 +0100

    update

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,7 +2,7 @@
 File              : init.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 30.11.2021
-Last Modified Date: 10.12.2021
+Last Modified Date: 15.12.2021
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 --]] --
 require('config')
