7ec59a8ee32cb9e739c0a2a845ae84df0d12074b
Anton Riedel <anton.riedel@tum.de>
2021-12-22 09:27:09 +0100

    Update

--- neovim/.config/nvim/after/ftplugin/tex.lua
+++ neovim/.config/nvim/after/ftplugin/tex.lua
@@ -2,9 +2,9 @@
 File              : tex.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 30.11.2021
-Last Modified Date: 30.11.2021
+Last Modified Date: 16.12.2021
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
---]]--
+--]] --
 vim.bo.makeprg = "latexmk -pdf"
 
 vim.bo.spelllang = "en_us,de"
@@ -14,5 +14,6 @@ vim.cmd([[
 augroup Format
         autocmd!
         autocmd BufWritePost * FormatWrite
+        autocmd BufWritePost * !bash -c "rm -rf \~formatting*"
 augroup END
 ]])
