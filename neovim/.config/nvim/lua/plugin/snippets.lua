ed48ff6085e827a90a5780c0a1565b2e6f7e8dea
Anton Riedel <anton.riedel@tum.de>
2022-05-19 10:47:11 +0200

    update

--- neovim/.config/nvim/lua/plugin/snippets.lua
+++ neovim/.config/nvim/lua/plugin/snippets.lua
@@ -2,7 +2,7 @@
 File              : snippets.lua
 Author            : Anton Riedel <anton.riedel@tum.de>
 Date              : 30.11.2021
-Last Modified Date: 20.04.2022
+Last Modified Date: 08.05.2022
 Last Modified By  : Anton Riedel <anton.riedel@tum.de>
 --]] --
 if packer_bootstrap then return end
@@ -14,6 +14,5 @@ local function prequire(...)
 end
 
 local luasnip = prequire('luasnip')
-local my_snippets = require('snippets')
-luasnip.snippets = my_snippets
+
 require("luasnip.loaders.from_vscode").lazy_load()
